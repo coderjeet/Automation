158e3c1e85b723e7cb824a7d0d3e3f7de73c3793
Prasanjeet <developer.prasanjeet@gmail.com>
2020-04-13 13:19:37 +0530

    sdsd

--- src/test/java/stepDefinations/Hooks.java
+++ src/test/java/stepDefinations/Hooks.java
@@ -9,13 +9,15 @@ public class Hooks {
 	@Before("@SmokeTest")
 	public void beforeValidation(){
 		System.out.println("Before test hook called....................");
-		System.out.println("New print statement...");
 	}
 	
 	@After("@MobileTest")
 	public void afterValidation(){
 		System.out.println("After test hook validation called...........");
 	}
+	
+	
+	fdshdhsgdhgshdg
 	
 	
 	
